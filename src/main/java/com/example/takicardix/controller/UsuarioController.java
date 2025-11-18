0db4f65c7e1f19409f703ffe9c6e5ac8502dd79d
Vnderr <ande.diazp@duocuc.cl>
2025-11-18 00:58:29 -0300

    Mejoras menores

--- src/main/java/com/example/takicardix/controller/UsuarioController.java
+++ src/main/java/com/example/takicardix/controller/UsuarioController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -73,7 +74,7 @@ public class UsuarioController {
         return ResponseEntity.ok(updatedUsuario);
     }
 
-    @PostMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<Usuario> updateParcialUsuario(@PathVariable Integer id,@RequestBody Usuario usuario) {
         usuario.setUsuario_id(id);
         Usuario updatedUsuario = usuarioService.partialUpdate(usuario);
