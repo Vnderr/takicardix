e466e333bd3c779010509325c938157e3deef1df
Vnderr <ande.diazp@duocuc.cl>
2025-11-21 21:28:13 -0300

    Mejoras

--- src/main/java/com/example/takicardix/config/WebConfig.java
+++ src/main/java/com/example/takicardix/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.example.takicardix.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class WebCongif {
+@Configuration
+public class WebConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
