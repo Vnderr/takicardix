b5f0d6835c685f7a44d2ad8515ee7669d1f22b67
Vnderr <ande.diazp@duocuc.cl>
2025-11-22 16:25:51 -0300

    cambios

--- src/main/java/com/example/takicardix/model/Marca.java
+++ src/main/java/com/example/takicardix/model/Marca.java
@@ -1,13 +1,11 @@
 package com.example.takicardix.model;
 
-import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,8 +24,4 @@ public class Marca {
 
     @Column(name = "nombre", length = 20, nullable = false)
     private String nombre;
-
-    @OneToMany(mappedBy = "marca") 
-    private List<Producto> productos;
-
 }
