f22a7c2ce5754b1721cb99ef182bba2052452864
Vnderr <ande.diazp@duocuc.cl>
2025-11-23 16:07:59 -0300

    Cambios

--- src/main/java/com/example/takicardix/model/Usuario.java
+++ src/main/java/com/example/takicardix/model/Usuario.java
@@ -16,6 +16,7 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Data
 @AllArgsConstructor
