45204ad146c1fc1a1dcf6bdad46b7f74ec430df5
Vnderr <ande.diazp@duocuc.cl>
2025-12-09 23:03:08 -0300

    Cambios Token

--- src/main/java/com/example/takicardix/config/SecurityConfig.java
+++ src/main/java/com/example/takicardix/config/SecurityConfig.java
@@ -2,8 +2,6 @@ package com.example.takicardix.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,11 +19,6 @@ public class SecurityConfig {
 
     public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter) {
         this.jwtAuthFilter = jwtAuthFilter;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
     }
 
     @Bean
